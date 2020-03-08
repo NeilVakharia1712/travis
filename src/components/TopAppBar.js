f17b80efc5b7374b4c971069775ecdbe51a51290
Neil Vakharia <NeilVakharia2021@u.northwestern.edu>
2020-03-07 21:42:41 -0600

    change to deploy

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -29,7 +29,7 @@ const TopAppBar = (props) => {
 		<React.Fragment>
 			<CssBaseline />
 			<ElevationScroll {...props}>
-				<AppBar style = {{background: "yellow"}}>
+				<AppBar style = {{background: "black"}}>
 					<Toolbar>
 						<AppDrawer userRole={props.userRole} setPage={props.setPage} />
 						<Grid container alignItems="center" justify="space-between">
