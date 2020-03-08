bc2b97db8de0009fe6049c85ba25b1f28a54d32c
Neil Vakharia <NeilVakharia2021@u.northwestern.edu>
2020-03-07 22:11:21 -0600

    fix fail

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { AppBar, Toolbar, Typography, useScrollTrigger, CssBaseline, Avatar, Button, Grid } from '@material-ui/core';
 import { signInWithGoogle } from '../utils/FirebaseAuthUtils';
 import LogoutPopover from './LogoutPopover';
